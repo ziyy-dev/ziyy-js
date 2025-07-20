7f5494ffbea91f169f6fe33f7165f594f2d25d44
alMukaafih <almukaafih@gmail.com>
2025-07-21 00:08:26 +0100

    Bump version of ziyy to 2.0.3
    Bump version of @ziyy/core to 0.1.5

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,14 @@
 
 #[macro_use]
 extern crate napi_derive;
-use ziyy_core::{Parser, Resolver, Splitter};
+use ziyy_core::try_style;
 
 #[napi]
 pub fn style(source: String) -> napi::Result<String> {
-    // let mut indexer = Indexer::new();
-    // let source = indexer.index(source.as_ref().to_string());
-    let mut splitter = Splitter::new();
-    #[allow(clippy::unnecessary_to_owned)]
-    let frags = match splitter.split(source) {
+    let output = match try_style(source) {
         Ok(v) => v,
         Err(e) => return Err(napi::Error::from_reason(e.to_string())),
     };
 
-    let parser = Parser::new(false);
-    let chunks = match parser.parse(frags) {
-        Ok(v) => v,
-        Err(e) => return Err(napi::Error::from_reason(e.to_string())),
-    };
-
-    let mut resolver = Resolver::new(false);
-    let output = match resolver.resolve(chunks) {
-        Ok(v) => v,
-        Err(e) => return Err(napi::Error::from_reason(e.to_string())),
-    };
-
-    let mut buf = String::new();
-    output.root().to_string(&mut buf);
-    Ok(buf)
+    Ok(output)
 }
